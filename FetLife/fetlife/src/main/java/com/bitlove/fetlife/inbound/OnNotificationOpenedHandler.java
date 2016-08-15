7461ea9bde2d153e5e9c6e4da1a06401411387e2
kistitan <kistitan@gmail.com>
2016-08-15 16:16:22 +0200

    small fix

--- FetLife/fetlife/src/main/java/com/bitlove/fetlife/inbound/OnNotificationOpenedHandler.java
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/inbound/OnNotificationOpenedHandler.java
@@ -11,7 +11,9 @@ public class OnNotificationOpenedHandler implements OneSignal.NotificationOpened
     @Override
     public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
         FetLifeApplication fetLifeApplication = FetLifeApplication.getInstance();
-
+        if (additionalData == null) {
+            return;
+        }
         OneSignalNotification oneSignalNotification = fetLifeApplication.getNotificationParser().parseNotification(fetLifeApplication, additionalData.optString(NotificationParser.JSON_FIELD_STRING_TITLE), message, additionalData.optString(NotificationParser.JSON_FIELD_STRING_LAUNCHURL), additionalData, null, additionalData.optString(NotificationParser.JSON_FIELD_STRING_GROUP));
         oneSignalNotification.onClick(fetLifeApplication);
     }
