5d1882e3af7a74dc62af2c3bf285095cc161f59f
kistitan <kistitan@gmail.com>
2017-05-23 16:27:24 +0200

    clear cache a update if needed

--- FetLife/fetlife/src/main/java/com/bitlove/fetlife/util/FileUtil.java
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/util/FileUtil.java
@@ -23,6 +23,23 @@ import java.util.List;
 
 public class FileUtil {
 
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
+    }
+
     public static void copyFileContent(File source, File target) {
         if (BuildConfig.DEBUG) {
             Log.e("FileUtil","Copying File content");
